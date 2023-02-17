25654a1815fb7308a7b9494b71c57c59657a4119
Thomas Porter <thomas.porter.1991@gmail.com>
2023-02-16 23:19:33 -0500

    step 39

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { SearchResults: [{name: '...', artist: '...', album: '...', id: ''}]};
+    this.state = { searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: '1'},
+                                   {name: 'name2', artist: 'artist2', album: 'album2', id: '2'},
+                                   {name: 'name3', artist: 'artist3', album: 'album3', id: '3'}],
+                   playlistName: 'My Playlist',
+                   playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: '1'},
+                                    {name: 'name2', artist: 'artist2', album: 'album2', id: '2'},
+                                    {name: 'name3', artist: 'artist3', album: 'album3', id: '3'}]};
+
   }
 
   render() {
@@ -16,8 +24,8 @@ export default class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} s/>
-            {"<!-- Add a Playlist component -->"}
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
