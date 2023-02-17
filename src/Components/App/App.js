c7bbf63c9a7d522f4cb42a022926ccbd38e28548
Thomas Porter <thomas.porter.1991@gmail.com>
2023-02-16 22:34:05 -0500

    step 33

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,27 @@
+import React from 'react';
 import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          {"<!-- Add a Playlist component -->"}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { SearchResults: [{name: '...', artist: '...', album: '...', id: ''}]};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} s/>
+            {"<!-- Add a Playlist component -->"}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
