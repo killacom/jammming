798424adc372daeadbb58797ba2b2f58529b9882
Thomas Porter <thomas.porter.1991@gmail.com>
2023-02-18 21:17:40 -0500

    step 36

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Playlist } from "../Playlist/Playlist";
 import './Track.css';
 
 export class Track extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
     renderAction() {
-        let sign;
-        this.props.isRemoval ? sign = "-" : sign = "+";
-        return <button className="Track-action">{sign}</button>;
+        if (this.props.isRemoval) {
+            return <button className="Track-action" onClick={this.removeTrack}>-</button>
+        } else {
+            return <button className="Track-action" onClick={this.addTrack}>+</button>
+        };
+    }
+    addTrack() {
+        this.props.onAdd(this.props.track);
+    }
+    removeTrack() {
+        this.props.onRemove(this.props.track);
     }
     render() {
         return (
