607b6e025f3f9a04061541feaee12dbf1aa3e187
Thomas Porter <thomas.porter.1991@gmail.com>
2023-02-22 20:44:32 -0500

    step 89 -searchResults working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
-
+import Spotify from '../../util/Spotify';
+Spotify.getAccessToken();
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -41,7 +42,9 @@ export default class App extends React.Component {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
   }
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    })
   }
   render() {
     return (
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -16,9 +16,8 @@ export class SearchBar extends React.Component {
     render() {
         return( 
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" 
-                       onChange={this.handleTermChange} />
-                <button className="SearchButton">SEARCH</button>
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onSubmit={this.search} />
+                <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
     }
